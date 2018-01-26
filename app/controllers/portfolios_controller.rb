d8659cf5b6a5e3ef91c9e21b85f42f2f290a42b8
Wnokes <westonroynokes@gmail.com>
2018-01-25 17:01:44 -0700

    did some stuff, broke some stuff fixed some stuff

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -9,10 +9,11 @@ class PortfoliosController < ApplicationController
 
 	def new
 		@portfolio_item = Portfolio.new
+    3.times { @portfolio_item.technologies.build }
 	end
 
   def create
-    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name]))
 
     respond_to do |format|
       if @portfolio_item.save
@@ -48,7 +49,7 @@ class PortfoliosController < ApplicationController
   def destroy
     # Perform the lookup
     @portfolio_item = Portfolio.find(params[:id])
-
+:name
     # Destroy/delete the record
     @portfolio_item.destroy
 
