f488230fc2a79317bd42a00380a4639c57f0b912
Wnokes <westonroynokes@gmail.com>
2018-02-01 14:58:17 -0700

    Made users its pretty awesome

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,12 +4,13 @@ module ApplicationHelper
 	# end
 
 	def login_helper
-      if current_user.is_a?(User) 
-       link_to "Logout", destroy_user_session_path, method: :delete 
-      else 
-       (link_to "Sign In", new_user_session_path) +
+      if current_user.is_a?(GuestUser) 
+        (link_to "Sign In", new_user_session_path) +
        "<br>".html_safe +
-	   (link_to "Sign Up", new_user_registration_path) 
+        (link_to "Sign Up", new_user_registration_path) 
+      else 
+       link_to "Logout", destroy_user_session_path, method: :delete 
+       
     end 
   end
 
