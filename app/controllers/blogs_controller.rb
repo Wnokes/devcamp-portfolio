c9b28d05abd37740978dbdd39dbcfb7103299328
Wnokes <westonroynokes@gmail.com>
2018-02-14 16:48:26 -0700

    added funtinality to only show published blogs to non admin users as well as have to most recent on top

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -6,7 +6,11 @@ class BlogsController < ApplicationController
   # GET /blogs
   # GET /blogs.json
   def index
-    @blogs = Blog.page(params[:page]).per(5)
+    if logged_in?(:site_admin)
+      @blogs = Blog.recent.page(params[:page]).per(5)
+    else
+      @blogs = Blog.published.recent.page(params[:page]).per(5)
+    end
     @page_title = "My Portfolio Blog"
   end
 
@@ -78,6 +82,7 @@ class BlogsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_blog
+      @blog = Blog.friendly.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
